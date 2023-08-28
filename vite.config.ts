3b82acdf5b690b26371d434fe1fd87588c897d87
Aleo <87682926+ALeoNya@users.noreply.github.com>
2023-08-28 13:10:28 +0800

    Add files via upload
    周末在家写的

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server:{
+    proxy:{
+       '/api':{
+           target:"http://localhost:8080/", //跨域地址
+           changeOrigin:true, //支持跨域
+           rewrite:(path) => path.replace(/^\/api/, "")//重写路径,替换/api
+       }
+    }
+ }
 })
